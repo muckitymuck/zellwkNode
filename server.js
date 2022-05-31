f0c43aaddad4f52bd876d8a17127032932242c22
muckity <andrewgoldstein@hotmail.com>
2022-05-31 05:58:27 -0500

    update mongoclient

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const MongoClient = require('mongodb').MongoClient
 const connectionString = "mongodb+srv://muckitymuck:hbwEBJS2pSG090SZ@cluster0.ny2v6.mongodb.net/?retryWrites=true&w=majority"
 //app.use(bodyParser.urlencoded({ extended: true}))
 //replace the above with this line
-app.use(express.urlencoded())
+// app.use(express.urlencoded())
 
-app.listen(3000, function() {
-    console.log('listening on 3000')
-})
+// app.listen(3000, function() {
+//     console.log('listening on 3000')
+// })
 
 //app.get(endpoint, callback)
 
@@ -20,15 +20,33 @@ app.listen(3000, function() {
 //     res.send('Hello World')
 // })
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html')
+// })
+// app.post('/quotes', (req, res) => {
+//     console.log(req.body)
+// })
 
-MongoClient.connect(connectionString, (err, client) => {
-    if (err) return console.error(err)
-    console.log('Connected to Database')    
-})
+// MongoClient.connect(connectionString, (err, client) => {
+//     if (err) return console.error(err)
+//     console.log('Connected to Database')    
+// })
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+    .then(client => {
+        console.log('Connected to Database')
+        const db = client.db('star-wars-quotes')
+        app.get('/', (req, res) => {
+            res.sendFile(__dirname + '/index.html')
+        })
+        app.post('/quotes', (req, res) => {
+            console.log(req.body)
+        })
+        app.use(express.urlencoded())
+
+        app.listen(3000, function() {
+            console.log('listening on 3000')
+        })
+    })
+    .catch(console.error)
     
